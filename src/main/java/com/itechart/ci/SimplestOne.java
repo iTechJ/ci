4714fc44bd4aca65b2a97a0d3f0d139fd8dc39ba
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2015-05-04 16:59:44 +0300

    new line

---
+++ src/main/java/com/itechart/ci/SimplestOne.java
@@ -0,0 +1,11 @@
+package com.itechart.ci;
+
+/**
+ * The simplest class, showing how CI works
+ * Step 1: add something, commit, check CI reaction
+ * Step 2: break something, commit, check CI reaction
+ */
+
+public class SimplestOne {
+    private final String breakMe = "break me";
+}
--- target/CiTest/index.jsp
+++ target/CiTest/index.jsp
@@ -1,6 +1,5 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<b> New Line </b>
 </body>
 </html>
